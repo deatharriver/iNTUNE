f95762768e3525c1be5ba4f7e8de2ee98457b9fe
Bhavesh Patil <85506638+deatharriver@users.noreply.github.com>
2022-02-02 15:13:54 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,87 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Chellakuttiye",
+    artist: "Jecin George and Pearle Maney",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Pashmina",
+    artist: "Amit Trivedi",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Dil Bechara",
+    artist: "A R Rahman",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Rabata",
+    artist: "Arjit Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Tare Ginn",
+    artist: "Mohit Chauhan",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Enjoy Enjami",
+    artist: "Yohani",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Inkem Inkem",
+    artist: "Sid Sriman",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Main Tumhara",
+    artist: "A R Rahman",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Agar Tum Na Hote",
+    artist: "Album",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Tera Ban Jaunga",
+    artist: "Arjit Singh",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "It's My Life",
+    artist: "Sandaru Sansthara",
+    img: "music-11",
+    src: "music-11"
+  },
+  
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
